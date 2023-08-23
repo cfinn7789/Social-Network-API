63fabeed4d5edc30e481a88b334efc86a0994c79
cfinn7789 <cfinn7789@gmail.com>
2023-08-23 19:08:03 -0400

    forgot to change db name

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 // Connects to mongodb
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialsDB';
 
 connect(connectionString);
 
