e101ebabd76b172ea1d42737fe3bdc0c6b702a7f
cfinn7789 <cfinn7789@gmail.com>
2023-08-23 14:59:51 -0400

    updated thoughts model

---
+++ models/thought.js
@@ -0,0 +1,52 @@
+// thoughtText, createdAt, username, reactions
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Types.objectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 300
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+// Schema for what makes up a thought
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 300,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+    },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+// Initializes the model
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
+  
